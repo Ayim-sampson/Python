b4dbc2938c24da6a9f99470ff1377d2f87b3a4a9
Sampson Ayim <146284547+Ayim-sampson@users.noreply.github.com>
2024-05-08 04:12:39 +0000

    Add files via upload

---
+++ for-loop.py
@@ -0,0 +1,8 @@
+"""
+Working with for Loops
+"""
+print("Welcome to Guess the Number!")
+print("The rules are simple. I will think of a number, and you will try to guess it.")
+print("Count to 10!")
+for x in range (0, 11):
+    print(x)
